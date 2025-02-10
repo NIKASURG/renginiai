2bbdcaf541405408bb4fef1336f711847bda91c0
Nikas Surgautas <Nsurgautas1@gmail.com>
2025-02-10 21:58:57 +0200

    renginiai

---
+++ src/firebase/index.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDGutSVQxXdN6kyH-mHHPFXQcZWtpGd4ug",
+  authDomain: "miestevykstantysrenginei.firebaseapp.com",
+  projectId: "miestevykstantysrenginei",
+  storageBucket: "miestevykstantysrenginei.firebasestorage.app",
+  messagingSenderId: "689045823394",
+  appId: "1:689045823394:web:d48b55e7c530f1b6b19cfb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
